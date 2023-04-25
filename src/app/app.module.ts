f8997abe525170731340c772a4e0ca92dffb6f09
Jankovics, Sandor Ferenc <SJankovics@graphisoft.com>
2023-04-25 09:12:12 +0200

    Add shell components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './shell/topbar/topbar.component';
+import { SidebarComponent } from './shell/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
